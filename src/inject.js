9055d85f370bb68e228dd0f1f815dc9ee91f68cf
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2018-02-22 16:50:27 +0900

    colorize

--- src/inject.js
+++ src/inject.js
@@ -1,7 +1,29 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   var href = request.href;
   if(href){
-    document.querySelector('link[rel*="icon"]').href = href
-    document.querySelector('link[rel*="shortcut icon"]').href = href
+    var link = document.querySelector('link[rel*="icon"]') || document.querySelector('link[rel*="shortcut icon"]');
+    if (!link) {
+      link = document.createElement("link");
+    }
+    link.setAttribute("rel", "shortcut icon");
+    document.head.appendChild(link);
+    function onImageLoaded() {
+      var canvas = document.createElement("canvas");
+      canvas.width = 16;
+      canvas.height = 16;
+      var context = canvas.getContext("2d");
+      context.drawImage(img, 0, 0);
+      context.globalCompositeOperation = "source-in";
+      context.fillStyle = request.color;
+      context.fillRect(0, 0, 16, 16);
+      context.fill();
+      link.type = "image/x-icon";
+      link.href = canvas.toDataURL();
+    };
+    var img = document.createElement("img");
+    img.crossOrigin = 'anonymous';
+    img.addEventListener("load", onImageLoaded);
+    img.src = href;
   }
 });
+
--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete') {
-    setFavicon(tabId, ((href) => {
+    setFavicon(tabId, ((href, color) => {
       const err = chrome.runtime.lastError;
       if (err){
 
       }else {
-        chrome.tabs.sendMessage(tabId, {href: href});
+        chrome.tabs.sendMessage(tabId, {href: href, color: color});
       }}));
   }
 });
 
 function setFavicon(tabId, callback){
   var index = tabId % 12;
+  var color = '#'+Math.floor(Math.random()*16777215).toString(16);
   var href = chrome.extension.getURL('assets/' + index + '.ico');
   chrome.tabs.executeScript(tabId, {
     file: './src/inject.js'
-  }, function(){ callback(href); });
+  }, function(){ callback(href, color); });
 }
