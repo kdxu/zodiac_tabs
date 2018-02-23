0d13f210940293200b8b467628e51b4a21ce0eb7
Kyoko KADOWAKI <kyoko.kadowaki@gmail.com>
2018-02-23 11:17:54 +0900

    dataUrlをキャッシュする

--- src/inject.js
+++ src/inject.js
@@ -1,24 +1,40 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  var href = request.href;
+  var links = document.querySelectorAll('link[rel*="icon"]', 'link[rel*="shortcut icon"]');
+  links.forEach(function(elem){elem.removeAttribute("rel");});
+  var host = new URL(request.url).host;
+  var index = request.tabId % 12;
+  var href = chrome.extension.getURL('assets/' + index + '.ico');
   if(href){
-    var links = document.querySelectorAll('link[rel*="icon"]', 'link[rel*="shortcut icon"]');
-    links.forEach(function(elem){elem.href = "";});
     var link = document.createElement("link");
-    document.head.appendChild(link);
     link.setAttribute("rel", "shortcut icon");
+    link.type = "image/x-icon";
+    document.head.appendChild(link);
+    var key = "zodiac-tab-data-of" + host;
+    // 新しく画像を作成
     function onImageLoaded() {
-      var canvas = document.createElement("canvas");
-      canvas.width = 16;
-      canvas.height = 16;
-      var context = canvas.getContext("2d");
-      context.drawImage(img, 0, 0);
-      context.globalCompositeOperation = "source-in";
-      context.fillStyle = request.color;
-      context.fillRect(0, 0, 16, 16);
-      context.fill();
-      link.type = "image/x-icon";
-      link.href = canvas.toDataURL();
-    };
+      var dataUrl;
+      chrome.storage.local.get(key, (items) => {
+        if (!items[key]){
+          var canvas = document.createElement("canvas");
+          canvas.width = 16;
+          canvas.height = 16;
+          var context = canvas.getContext("2d");
+          context.drawImage(img, 0, 0);
+          context.globalCompositeOperation = "source-in";
+          var color = '#'+Math.floor(Math.random()*16777215).toString(16);
+          context.fillStyle = color;
+          context.fillRect(0, 0, 16, 16);
+          context.fill();
+          dataUrl = canvas.toDataURL();
+          chrome.storage.local.set({[key]: {dataUrl: dataUrl}});
+        }
+        else{
+          dataUrl = items[key].dataUrl;
+        }
+        link.type = "image/x-icon";
+        link.href = dataUrl;
+      });
+    }
     var img = document.createElement("img");
     img.crossOrigin = 'anonymous';
     img.addEventListener("load", onImageLoaded);
--- src/main.js
+++ src/main.js
@@ -1,30 +1,15 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === 'complete') {
-    setFavicon(tabId, tab.url, ((href, color) => {
+    setFavicon(tabId, (() => {
       const err = chrome.runtime.lastError;
       if(!err){
-        chrome.tabs.sendMessage(tabId, {href: href, color: color});
+        chrome.tabs.sendMessage(tabId, {tabId: tabId, url: tab.url});
       }}));
   }
 });
 
-function setFavicon(tabId, url, callback){
-  var host = new URL(url).host;
-  var index = tabId % 12;
-  var color = "black";
-  var key = "zodiac-tab-color-of" + host;
-  var href;
-  chrome.storage.local.get(key, (items) => {
-    if (!items[key]){
-      color = '#'+Math.floor(Math.random()*16777215).toString(16);
-      href = chrome.extension.getURL('assets/' + index + '.ico');
-      chrome.storage.local.set({[key]: {color: color, href: href}});
-    }else{
-      color = items[key].color;
-      href = items[key].href;
-    }
-    });
+function setFavicon(tabId, callback){
   chrome.tabs.executeScript(tabId, {
     file: './src/inject.js'
-  }, function(){ callback(href, color); });
+  }, function(){ callback(); });
 }
